6678d33c4796a6e796319d0e0dc4dab181450b15
hopkinslaurel <hopkins.laurel@gmail.com>
2017-06-06 10:38:00 -0700

    Initial commit
    Working condition except after taking an action the state is not
    updated. Belief update also needs to be tuned

---
+++ policy_switching.py
@@ -0,0 +1,75 @@
+# Final Project, Laurel Hopkins & Eugene Seo
+# CS 533, Spring 2017
+
+from collections import *
+import numpy as np
+from random import randint
+import simulator as sim
+import xlsxwriter
+import csv
+import matplotlib.pyplot as plt
+
+'USER DEFINED PARAMETERS'
+num_models = 2  # number of expert models
+num_states = 4  # number of states
+policy_length = 8
+w = 4  # run sim(s, policy, h) w times for each policy
+
+
+# chooses expert based on belief state distribution
+def _belief(b, num_models):
+	threshold, count = [], 0
+	choose = randint(1, 100)
+	for i in range(0, num_models):
+		count += b[i] * 100
+		threshold.append(count)
+	for i in range(0, num_models):
+		if choose <= threshold[i]:
+			return i
+
+
+def policy_switching(state, expert, w, policy, horizon, rewards):
+	num_policies = policy.shape[0]
+	value = np.zeros(num_policies)
+	for i in range(0, num_policies):  # iterate through each policy
+		val = 0
+		for j in range(0, w):  # perform n
+			val += sim.simulator(state, policy[i], horizon, expert, rewards)
+		value[i] = val
+
+	return np.argmax(value)
+
+
+def update_models(state, expert_models, p, horizon, running_value, num_models):
+	value = np.zeros(num_models)
+	for i in range(0, num_models):
+		value[i] = sim.simulator(state, p, horizon, expert_models[i], rewards)
+		running_value[i] += value[i]
+	return np.argmax(value), running_value
+
+
+def update_belief(belief, opt_model):
+	if optimal_model == 0:
+		belief[0] += 0.05
+		belief[1] -= 0.05
+	return belief
+
+
+if __name__ == '__main__':
+	state, output_policy, best_expert = 0, [], []  # start in state 0 -- could change to another state
+	running_value = np.zeros(num_models)
+	expert_models, policy, rewards, belief = sim.init_models(num_models, num_states)
+
+	for horizon in range(0, policy_length):
+		ex = _belief(belief, num_models)  # returns expert model to use
+		p = policy_switching(state, expert_models[ex], w, policy, horizon, rewards)  # best policy from policy switching
+		output_policy.append(policy[p][horizon])
+		optimal_model, running_value = update_models(state, expert_models, policy[p], horizon, running_value, num_models)
+		best_expert.append(optimal_model)  # returns expert model with highest value after applying action from best policy
+		update_belief(belief, optimal_model)  # update belief
+	# how do we want to update belief state? increment best expert and decrement others equally?
+
+	print 'best_expert = ', best_expert
+	print
+	print 'output_policy = ', output_policy
+
---
+++ simulator.py
@@ -0,0 +1,107 @@
+# Final Project, Laurel Hopkins & Eugene Seo
+# CS 533, Spring 2017
+
+from random import randint
+from collections import *
+import numpy as np
+
+
+def expert0_init(states):
+	ex0 = defaultdict(list)
+
+	# Action 1
+	act1 = np.zeros((states, states))
+	for i in range(0, states):
+		act1[i][i] = 1
+	ex0[1] = act1
+
+	# Action 2
+	act2 = np.zeros((states, states))
+	for i in range(0, states):
+		if i == (states - 1):
+			act2[i][0] = 0.5
+			act2[i][i] = 0.5
+		else:
+			act2[i][i] = 0.5
+			act2[i][i+1] = 0.5
+	ex0[2] = act2
+	return ex0
+
+
+def expert1_init(states):
+	ex1 = defaultdict(list)
+
+	# Action 1
+	act1 = np.zeros((states, states))
+	for i in range(0, states):
+		if i == (states - 1):
+			act1[i][0] = 0.5
+			act1[i][i] = 0.5
+		else:
+			act1[i][i] = 0.5
+			act1[i][i + 1] = 0.5
+	ex1[1] = act1
+
+	# Action 2
+	act2 = np.zeros((states, states))
+	for i in range(0, states):
+		act2[i][i] = 1
+	ex1[2] = act2
+	return ex1
+
+
+def policy_init():
+	policy = np.zeros((3, 8))
+	policy[0] = [1, 1, 1, 2, 2, 2, 2, 2]
+	policy[1] = [2, 2, 2, 2, 2, 1, 1, 1]
+	policy[2] = [1, 2, 1, 2, 1, 2, 1, 2]
+	return policy
+
+
+def next_state(transition):
+	states, threshold, count = len(transition), [], 0
+	choose = randint(1, 100)
+	for i in range(0, states):
+		if count == 100.0:
+			threshold.append(0)
+		else:
+			count += transition[i]*100
+			threshold.append(count)
+	for i in range(0, states):
+		if choose <= threshold[i]:
+			return i
+
+
+def simulator(s, policy, h, expert, reward):
+	# return value from taking action defined by the policy for the given state and horizon
+	action = policy[h]
+	transition = expert[action][s]   # returns transition function [1, 0, 0, 0]
+	s_next = next_state(transition)
+	return s_next, reward[s_next]
+
+
+def reward_init(states):
+	rewards = np.zeros(states)
+	for i in range(0, states):
+		rewards[i] = 5 + i*5
+	return rewards
+
+
+def init_models(num_models, num_states):
+	expert_models = defaultdict(list)
+	ex0 = expert0_init(num_states)
+	ex1 = expert1_init(num_states)
+	expert_models[0] = ex0
+	expert_models[1] = ex1
+	policy = policy_init()
+	rewards = reward_init(num_states)
+	belief = np.zeros(num_models)
+
+	for i in range(0, num_models):
+		belief[i] = 1/float(num_models)
+
+	return expert_models, policy, rewards, belief
+
+
+if __name__ == '__main__':
+	init_models(2, 4)
